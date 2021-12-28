61ff01ea49494ffad6de86a05e806d59d15361ff
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2021-12-28 12:02:40 -0600

    Updated WS-AVD with more registry keys, updated installers.

--- AVD.ps1
+++ AVD.ps1
@@ -16,16 +16,24 @@ $dest = "c:\apps\ninite.exe"
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $dest
 # Source URL
-$url = "https://mattmckenzie.co/setup/FortiClient.exe"
-# Destation file
-$dest = "c:\apps\forticlient.exe"
-# Download the file
-Invoke-WebRequest -Uri $url -OutFile $dest
-# Source URL
 $url = "https://aka.ms/fslogix/download"
 # Destation file
 mkdir C:\apps\FSlogix
 $dest = "c:\apps\FSLogix\FSLogix.zip"
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $dest
+# Source URL
+$url = "https://aka.ms/OneDriveWVD-Installer"
+# Destation file
+mkdir C:\apps\FSlogix
+$dest = "c:\apps\OneDriveSetup.exe"
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $dest
+# Source URL
+$url = "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true"
+# Destation file
+mkdir C:\apps\FSlogix
+$dest = "c:\apps\Teams.msi"
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $dest
 
@@ -43,9 +51,6 @@ Set-Location C:\apps\
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-# Install Forticlient VPN Software
-.\forticlient.exe /quiet /log"Log.txt"
-
 # FSLogix Registry
 Expand-Archive -LiteralPath 'C:\apps\fslogix\fslogix.zip' -DestinationPath C:\apps\fslogix\
 Set-Location C:\apps\FSlogix\x64\Release
@@ -55,3 +60,35 @@ This is text which contains newlines
 It can also contain "quoted" strings
 "@
 $newValue.multistring
+
+# Timezone Redirection
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fEnableTimeZoneRedirection /t REG_DWORD /d 1 /f
+
+# Disable Storage Sense
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy" /v 01 /t REG_DWORD /d 0 /f
+
+# Enable Multi Session Win 10 Telemetry
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_DWORD /d 3 /f
+
+# O365 Registry Changes
+reg load HKU\TempDefault C:\Users\Default\NTUSER.DAT
+reg add HKU\TempDefault\SOFTWARE\Policies\Microsoft\office\16.0\common /v InsiderSlabBehavior /t REG_DWORD /d 2 /f
+reg add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v enable /t REG_DWORD /d 1 /f
+reg add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v syncwindowsetting /t REG_DWORD /d 1 /f
+reg add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v CalendarSyncWindowSetting /t REG_DWORD /d 1 /f
+reg add "HKU\TempDefault\software\policies\microsoft\office\16.0\outlook\cached mode" /v CalendarSyncWindowSettingMonths  /t REG_DWORD /d 1 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate /v hideupdatenotifications /t REG_DWORD /d 1 /f
+reg add HKLM\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate /v hideenabledisableupdates /t REG_DWORD /d 1 /f
+
+# OneDrive Setup
+Set-Location C:\apps\
+"C:\Apps\OneDriveSetup.exe" /uninstall
+REG ADD "HKLM\Software\Microsoft\OneDrive" /v "AllUsersInstall" /t REG_DWORD /d 1 /reg:64
+Run "C:\apps\OneDriveSetup.exe" /allusers
+REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "SilentAccountConfig" /t REG_DWORD /d 1 /f
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "<your-AzureAdTenantId>" /f
+
+# Teams Install - Machine Install
+reg add "HKLM\SOFTWARE\Microsoft\Teams" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
+msiexec /i C:\apps\Teams.msi /l*v C:\apps\Teams.log ALLUSER=1 ALLUSERS=1
