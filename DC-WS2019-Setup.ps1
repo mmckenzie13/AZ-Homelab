b228164fd63caccebecaee69b4564ad3738ba460
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2021-12-30 09:32:59 -0600

    Testing More Changes

--- DC-WS2019-Setup.ps1
+++ DC-WS2019-Setup.ps1
@@ -1,17 +1,16 @@
 $vmName = Read-Host -Prompt "VM Name"
-$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
 $location = Read-Host -Prompt "Enter the location (i.e. centralus)"
 $adminUsername = Read-Host -Prompt "Enter the administrator username"
 $adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
 $dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP (lowercase)"
 
-New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
-    -ResourceGroupName $resourceGroupName `
+    -ResourceGroupName "CORE-RG" `
     -TemplateUri "https://raw.githubusercontent.com/mmckenzie13/AZ-Homelab/main/DC-WS2019-Test.json" `
     -adminUsername $adminUsername `
     -adminPassword $adminPassword `
     -dnsLabelPrefix $dnsLabelPrefix `
     -vmName $vmName
+
 
  (Get-AzVm -ResourceGroupName $resourceGroupName).name
