798020554ed94afa820dc556771799a0cfe50fd1
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2021-12-30 11:01:24 -0600

    Template Rebuild

--- DC-WS2019-Setup.ps1
+++ DC-WS2019-Setup.ps1
@@ -1,15 +1,26 @@
-$vmName = Read-Host -Prompt "VM Name"
-$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
-$adminUsername = Read-Host -Prompt "Enter the administrator username"
-$adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
-$dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP (lowercase)"
+## Create the Availability Set for the 2 DCs
+New-AzAvailabilitySet -ResourceGroupName "CORE-RG" -Name "DC-AS" -Location "East US" -PlatformFaultDomainCount 2 -PlatformUpdateDomainCount 5 -Sku Aligned
+
+## Create DC01
+$adminUsername = Read-Host -Prompt "Enter the local administrator username"
+$adminPassword = Read-Host -Prompt "Enter the local administrator password" -AsSecureString
 
 New-AzResourceGroupDeployment `
     -ResourceGroupName "CORE-RG" `
-    -TemplateUri "https://raw.githubusercontent.com/mmckenzie13/AZ-Homelab/main/DC-WS2019-Test.json" `
+    -TemplateUri "https://raw.githubusercontent.com/mmckenzie13/AZ-Homelab/main/DC-WS2019-Template.json" `
     -adminUsername $adminUsername `
     -adminPassword $adminPassword `
     -dnsLabelPrefix $dnsLabelPrefix `
-    -vmName $vmName `
+    -vmName DC01
 
+## Create DC02
+$adminUsername = Read-Host -Prompt "Enter the local administrator username"
+$adminPassword = Read-Host -Prompt "Enter the local administrator password" -AsSecureString
 
+New-AzResourceGroupDeployment `
+    -ResourceGroupName "CORE-RG" `
+    -TemplateUri "https://raw.githubusercontent.com/mmckenzie13/AZ-Homelab/main/DC-WS2019-Template.json" `
+    -adminUsername $adminUsername `
+    -adminPassword $adminPassword `
+    -dnsLabelPrefix $dnsLabelPrefix `
+    -vmName DC01
