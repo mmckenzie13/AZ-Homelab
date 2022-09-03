589f762102a8a50d4cbd515d864f584c6a61eff1
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2022-09-02 22:20:05 -0500

    Added the actual execution of FSLogix

--- AVD/AVD-FSLogix.ps1
+++ AVD/AVD-FSLogix.ps1
@@ -17,5 +17,9 @@ $dest = "c:\apps\FSLogix\FSLogix.zip"
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $dest
 
+# FSLogix Install (Not Updated Often, uncomment to update, will restart the session host.)
+Expand-Archive -LiteralPath 'C:\apps\fslogix\fslogix.zip' -DestinationPath C:\apps\fslogix\
+Set-Location C:\apps\FSlogix\x64\Release
+.\FSLogixAppsSetup.exe /quiet
+
 Start-Sleep -Seconds 180
-Restart-Computer
