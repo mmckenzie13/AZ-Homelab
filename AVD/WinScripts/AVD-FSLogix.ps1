56d9ac779df3c2267e83a221bbcca25551702d05
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2023-10-03 11:28:09 -0500

    Updated to include build name in the folder extracted

--- AVD/WinScripts/AVD-FSLogix.ps1
+++ AVD/WinScripts/AVD-FSLogix.ps1
@@ -5,7 +5,7 @@ mkdir C:\apps
 Set-Location C:\apps\
 
 # Clear existing Files under C:\Apps if it exists
-get-childitem C:\apps\* -Recurse | Remove-item -Confirm:$false -Recurse
+get-childitem C:\apps\fslogix\* -Recurse | Remove-item -Confirm:$false -Recurse
 
 ## Download Files
 
@@ -19,7 +19,10 @@ Invoke-WebRequest -Uri $url -OutFile $dest
 
 # FSLogix Install (Not Updated Often, uncomment to update, will restart the session host.)
 Expand-Archive -LiteralPath 'C:\apps\fslogix\fslogix.zip' -DestinationPath C:\apps\fslogix\
-Set-Location C:\apps\FSlogix\x64\Release
+Set-Location C:\apps\fslogix\
+$fslogix = get-childitem | ?{ $_.PSIsContainer }
+$fslogix = "C:\apps\fslogix\" + $fslogix.name + "\x64\Release"
+Set-Location $fslogix
 .\FSLogixAppsSetup.exe /quiet
 
 Start-Sleep -Seconds 180
